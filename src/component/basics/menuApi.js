363c1b73548dccf31ec0b3e53b2ec73e23c4025e
Abhayay42 <abhayth1441@gmail.com>
2023-04-11 10:08:57 +0530

    first commit

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi ! In 1882, at a meeting of the Swiss “Non-Profit Society”, the doctor and factory inspector Fridolin Schuler spoke about the miserable nutritional situation of the factory workers: ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakora is a common street food found all over India and Pakistan and they are certainly one of my favourite appetisers to make. Not only because they are so simple to make, but because they taste so good!",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "corn, (Zea mays), also called Indian corn or maize, cereal plant of the grass family (Poaceae) and its edible grain. The domesticated crop originated in the Americas and is one of the most widely distributed of the world’s food crops.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chole Bhature also known as Chana Bhatura is one of the most popular Punjabi dish liked almost all over India. Chole stands for a spiced tangy chickpea curry and Bhatura is a soft and fluffy fried leavened bread.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Foods similar to pizza have been made since the Neolithic Age.[23] Records of people adding other ingredients to bread to make it more flavorful can be found throughout ancient history. In the 6th century BC, the Persian soldiers of the Achaemenid Empire during",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "According to archeologist Bindeshwari Prasad Sinha, dish-on-stand and simple dishes belonging to the Indus Valley Civilization maybe regarded as the prototype of Indian dishes as thalis . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Candy, also called sweets (British English) or lollies (Australian English, New Zealand English),[a] is a confection that features sugar as a principal ingredient. The category, called sugar . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Chawal (transl. Red kidney beans rice) is a 2018 Hindi-language Indian comedy-drama film directed by Leena Yadav, produced by Gulab Singh Tanwar and Aseem Bajaj .",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "A samosa (/səˈmoʊsə/) or singara is a fried South Asian[2] pastry with a savoury filling, including ingredients such as spiced potatoes, onions, and peas. It may take .",
+    },
+  ];
+  
+  export default Menu;
+  
